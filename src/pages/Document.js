410cd3d10b16e563f91ad211383c562236b3f1ef
Guilherme Mattoso <85652034+mattosoguilherme@users.noreply.github.com>
2024-10-15 16:55:34 -0300

    ..

--- src/pages/Document.js
+++ src/pages/Document.js
@@ -1,10 +1,12 @@
+import React from "react";
+
 const Document = () => {
-    return (
-        <div>
-            <h1>Terms and Conditions</h1>
-            <p>Welcome to our terms and services page.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>Welcome to our terms and services page.</p>
+    </div>
+  );
 };
 
 export default Document;
